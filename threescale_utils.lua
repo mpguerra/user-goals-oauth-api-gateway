905c2993f65d6353f2ec9c1755644b3eb1746ebd
Maria Pilar Guerra Arias <pili@3scale.net>
2014-06-13 17:47:20 +0200

    put data in directly

--- threescale_utils.lua
+++ threescale_utils.lua
@@ -86,22 +86,20 @@ end
 
 function M.connect_redis(red)
   ngx.log(ngx.STDERR, 'connecting to redis')
-  redisurl = os.getenv("REDISTOGO_URL")
-  ngx.say("redisurl: "..redisurl)
-  redisurl_connect = string.split(redisurl, ":")[3]
-  ngx.say("redisurl_connect_string: "..redisurl_connect)
-  redisurl_user = string.split(redisurl_connect, "@")[1]
-  redisurl_host = string.split(redisurl_connect, "@")[2]
-  redisurl_port = string.split(redisurl, ":")[4]
-  ngx.log(ngx.STDERR, redisurl)
+  --redisurl = os.getenv("REDISTOGO_URL")
+  --redisurl_connect = string.split(redisurl, ":")[3]
+  --redisurl_user = string.split(redisurl_connect, "@")[1]
+  --redisurl_host = string.split(redisurl_connect, "@")[2]
+  --redisurl_port = string.split(redisurl, ":")[4]
+  --ngx.log(ngx.STDERR, redisurl)
    --local ok, err = red:connect("127.0.0.1", 6379)
-  local ok, err = red:connect(redisurl_host, redisurl_port)
+  local ok, err = red:connect("viperfish.redistogo.com", 9191)
   if not ok then
     ngx.say("failed to connect: ", err)
     ngx.exit(ngx.HTTP_OK)
   end
 
-  local res, err = red:auth(redisurl_user)
+  local res, err = red:auth("0925e54ca0456ef7818ae3b97e90c6d6")
   if not res then
     ngx.say("failed to authenticate: ", err)
     return
